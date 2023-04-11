a1c966395bd2f5f256a18e5b824478a4fb73e50e
ant07hony <ant07hony@yahoo.com>
2023-04-11 14:45:54 -0400

    Initial setup with Django and connect to database and .env for secret key

--- finchcollector/finchcollector/settings.py
+++ finchcollector/finchcollector/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w#16z*md8=vb*j2dl_n+$r2zs0w4bf^135*gci0qrf+1ek7p%a'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
